577ca6a87c0ee4c12c0121e7b962c7759b628ea6
Artem <Artem@DESKTOP-A585VQ9>
2023-02-01 22:04:50 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,132 @@
+﻿
+using ConsoleApp2;
+
+List<Сотрудник> Сотрудники = new List<Сотрудник>();
+
+while (true)
+{
+    bool Sort_positionatwork = false;
+    int []SortType = null;
+    Console.WriteLine("Выберите действие:");
+    Console.WriteLine("1. Добавить сотрудника(Простой вариант)");
+    Console.WriteLine("2. Добавить сотрудника(Подробный вариант)");
+    Console.WriteLine("3. Вывести всех сотрудников");
+    Console.WriteLine("4. Подсчитать возраст каждого из них");
+    Console.WriteLine("5. Отсортировать по должностям");
+    Console.Write("\nВыбор: ");
+
+   string inputString = Console.ReadLine();
+    try
+    {
+        string Выбор = inputString;
+
+        switch (Выбор)
+        {
+            case "1":
+                {
+                    Console.Write("Введите фамилию: ");
+                    string familia = Console.ReadLine();
+
+                    Console.Write("Введите имя: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Введите дату рождения (YYYY.MM.DD): ");
+                    string age = Console.ReadLine();
+
+                    Сотрудник Новичек = new Сотрудник(familia, name, null, age, null, null);
+                    Сотрудники.Add(Новичек);
+                    Sort_positionatwork = false;
+                    break;
+                }
+            case "2":
+                {
+                    Console.Write("Введите фамилию: ");
+                    string familia = Console.ReadLine();
+
+                    Console.Write("Введите имя: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Введите Ваш Гендер: ");
+                    string sex = Console.ReadLine();
+
+                    Console.Write("Введите дату рождения (YYYY.MM.DD): ");
+                    string age = Console.ReadLine();
+
+                    Console.Write("Введите Вашу должность:  ");
+                    string positionatwork = Console.ReadLine();
+
+                    Console.Write("Введите дату устройства на работу (YYYY.MM.DD): ");
+                    string timeofworking = Console.ReadLine();
+
+                    Сотрудник Новичек = new Сотрудник(familia, name, sex, age, positionatwork, timeofworking);
+                    Сотрудники.Add(Новичек);
+                    Sort_positionatwork = false;
+                    break;
+                }
+            case "3":
+                {
+                    if (Сотрудники.Count == 0)
+                        Console.WriteLine("В базе нет сотрудников.");
+                    else
+                    {
+                        if (SortType == null)
+                            for (int i = 0; i < Сотрудники.Count; i++)
+                                Console.WriteLine(Сотрудники[i].Print());
+                       else
+                        for (int i = 0; i < SortType.Length; i++)
+                            Console.WriteLine(Сотрудники[SortType[i]].Print());
+                    }
+                    break;
+                }
+            case "4":
+                {
+                    if (Сотрудники.Count == 0)
+                        Console.WriteLine("В базе нет сотрудников.");
+                    else
+                    {
+                        for (int i = 0; i < Сотрудники.Count; i++)
+                        {
+                            if (Сотрудники[i].GetAge() != 0)
+                            Console.WriteLine($"{Сотрудники[i].GetFullName()}: {Сотрудники[i].GetAge()}"); 
+                            else Console.WriteLine($"{Сотрудники[i].GetFullName()}: Возраст не указан."); 
+                        }
+                            
+                    }
+                    break;
+                }
+            //case "5":
+            //    {
+            //        if (Сотрудники.Count == 0)
+            //            Console.WriteLine("В базе нет сотрудников.");
+            //        else
+            //        {
+            //            SortType = new int[Сотрудники.Count];
+            //            for (int temp = 0; temp < Сотрудники.Count * 100; temp++)
+            //            {
+            //                for (int j = 0; j < Сотрудники.Count - 1; j++)
+            //                {
+            //                    string x = Сотрудники[j].Getpositionatwork();
+            //                    string y = Сотрудники[j+1].Getpositionatwork();
+            //                    if (x.Equals(y))
+            //                    {
+                                    
+            //                    }
+            //                }
+            //            }
+            //        }
+            //        break;
+            //    }
+            default:
+                {
+                    Console.WriteLine("Такой вариант не предусмотрен.");
+                    break;
+                }
+        }
+        Console.WriteLine("Нажмите любую клавишу для продолжения...");
+        Console.ReadLine();
+    }
+    finally
+    {
+        Console.Clear();
+    }
+}
---
+++ ConsoleApp2/Сотрудник.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Сотрудник
+    {
+        private string Фамилия;
+        private string Имя;
+        private string? Отчество;
+        private DateTime Дата_Рождения;
+        private string? Должность;
+        private string? Телефон;
+        // public static string[] Список_Должностей = new string[] { "Директор", "Лох", "Пидр"};
+        private string? Гендер;
+        private DateTime Момент_приема_на_работу;
+
+        public Сотрудник(string Фамилия, string Имя, string Гендер, string Дата_Рождения, string Должность1, string Момент_приема_на_работу)
+        {
+            this.Фамилия = Фамилия;
+            this.Имя = Имя;
+            this.Гендер = Гендер;
+
+            try
+            {
+                this.Дата_Рождения = DateTime.Parse(Дата_Рождения);
+            }
+            catch
+            {
+                this.Дата_Рождения = new DateTime(0001, 1, 1);
+            }
+
+            this.Должность = Должность;
+
+            try
+            {
+                this.Момент_приема_на_работу = DateTime.Parse(Момент_приема_на_работу);
+            }
+            catch
+            {
+                this.Момент_приема_на_работу = new DateTime();
+            }
+
+            Отчество = null;
+            Телефон = null;
+        }
+        //public Сотрудник(string Фамилия, string Имя, string Отчество, string Дата_Рождения, string Должность) //, string Телефон)
+        //{
+        //    this.Фамилия = Фамилия;
+        //    this.Имя = Имя;
+        //    this.Отчество = Отчество;
+
+
+        //    if (Список_Должностей.ToList().IndexOf(Должность) == -1)
+        //    {
+        //        this.Должность = null;
+        //    }
+        //    else
+        //        this.Должность = Должность;
+
+        //    // this.Телефон = Телефон;
+
+        //    try
+        //    {
+        //        this.Дата_Рождения = DateTime.Parse(Дата_Рождения);
+        //    }
+        //    catch
+        //    {
+        //        this.Дата_Рождения = new DateTime(0001, 1, 1);
+        //    }
+        //}
+
+        public string Print()
+        {
+            string Output = $"Фамилия: {Фамилия}\nИмя: {Имя}\n";
+            if (Отчество != null)
+                Output += $"Отчество: {Отчество}\n";
+            if (Гендер != null)
+                Output += $"Гендер: {Гендер}\n";
+
+            if (Дата_Рождения.Year != 0001)
+            {
+                Output += "Дата Рождения: ";
+                Output += Дата_Рождения.ToShortDateString() + "\n";
+            }
+            if (Должность != null)
+                Output += $"Должность: {Должность}\n";
+            if (Момент_приема_на_работу.Year != 0001)
+            {
+                Output += "Момент приема на работу: ";
+                Output += Момент_приема_на_работу.ToShortDateString();
+            }
+            if (Должность != null)
+                Output += $"Должность: {Должность}\n";
+            if (Телефон != null)
+                Output += $"Телефон: {Телефон}\n";
+
+            return Output + "\n";
+        }
+        public int GetAge()
+        {
+            if (Дата_Рождения.Year == 0001)
+                return 0;
+            int Age = DateTime.Now.Year - Дата_Рождения.Year;
+            if (DateTime.Now.Month < Дата_Рождения.Month || DateTime.Now.Month == Дата_Рождения.Month && DateTime.Now.Day < Дата_Рождения.Day) 
+            Age--;
+            return Age;
+        }
+
+        public string Getpositionatwork()
+        {
+            return Должность;
+        }
+        public string GetFullName()
+        {
+            return $"{Фамилия} {Имя} {Отчество}";
+        }
+        public void SetФамилия(string Value) { Фамилия = Value; }
+        public void SetИмя(string Value) { Имя = Value; }
+        public void SetОтчество(string Value) { Отчество = Value; }
+        public void SetДата_Рождения(string Value) { Дата_Рождения = DateTime.Parse(Value); }
+        //public void SetДолжность(string Value) {
+        //    if (Список_Должностей.ToList().IndexOf(Value) == -1)
+        //    {
+        //        this.Должность = null;
+        //    }
+        //    else
+        //        this.Должность = Должность;
+        //}
+        public void SetТелефон(string Value) { Телефон = Value; }
+    }
+}
